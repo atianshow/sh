e66065bd4fec4c9ef46c324203005484b3784493
阿天 <112182068+atianshow@users.noreply.github.com>
2024-05-07 23:22:48 +0800

    修复错误

--- install.sh
+++ install.sh
@@ -7,58 +7,112 @@ is_command_available() {
 
 # Function to check if Docker is installed
 is_docker_installed() {
-  docker --version &> /dev/null
+  if is_command_available docker; then
+    docker --version &> /dev/null
+  else
+    return 1
+  fi
 }
 
 # Function to check if Docker Compose is installed
 is_docker_compose_installed() {
-  docker-compose --version &> /dev/null
+  if is_command_available docker-compose; then
+    docker-compose --version &> /dev/null
+  else
+    return 1
+  fi
 }
 
 # Function to check if Portainer is installed
 is_portainer_installed() {
-  docker ps -f name=portainer &> /dev/null
+  if is_command_available docker; then
+    docker inspect portainer &> /dev/null
+  else
+    return 1
+  fi
 }
 
 # Function to check if Nginx Proxy Manager is installed
 is_nginx_proxy_manager_installed() {
-  docker ps -f name=npm &> /dev/null
+  if is_command_available docker; then
+    docker inspect nginx-proxy-manager &> /dev/null
+  else
+    return 1
+  fi
 }
 
 # Function to check if ServerStatus is installed
 is_serverstatus_installed() {
-  docker ps -f name=serverstatus &> /dev/null
+  if is_command_available docker; then
+    docker inspect serverstatus &> /dev/null
+  else
+    return 1
+  fi
 }
 
 # Function to install Docker
 install_docker() {
-  install_package docker.io
-  sudo usermod -aG docker "$USER"
+  if ! is_docker_installed; then
+    if is_command_available apt-get; then
+      sudo apt-get update
+      sudo apt-get install -y docker.io
+    elif is_command_available yum; then
+      sudo yum install -y docker
+    elif is_command_available dnf; then
+      sudo dnf install -y docker
+    elif is_command_available zypper; then
+      sudo zypper install -y docker
+    else
+      echo "Unsupported package manager."
+      return 1
+    fi
+
+    sudo usermod -aG docker "$USER"
+  fi
 }
 
 # Function to install Docker Compose
 install_docker_compose() {
-  sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  if ! is_docker_compose_installed; then
+    if is_command_available curl; then
+      sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
+    else
+      echo "curl is not installed."
+      return 1
+    fi
+  fi
 }
 
 # Function to install Portainer
 install_portainer() {
-  docker volume create portainer_data
-  docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+  if ! is_portainer_installed; then
+    install_docker
+
+    docker volume create portainer_data
+    docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+  fi
 }
 
 # Function to install Nginx Proxy Manager
 install_nginx_proxy_manager() {
-  docker volume create npm_data
-  docker run -d -p 80:80 -p 443:443 -p 81:81 --name=npm --restart=always -v npm_data:/data -v ./letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
+  if ! is_nginx_proxy_manager_installed; then
+    install_docker
+
+    docker volume create npm_data
+    docker run -d -p 80:80 -p 443:443 -p 81:81 --name=npm --restart=always -v npm_data:/data -v ./letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
+  fi
 }
 
 # Function to install ServerStatus
 install_serverstatus() {
-  wget --no-check-certificate -qO ~/serverstatus-config.json https://raw.githubusercontent.com/cppla/ServerStatus/master/server/config.json && \
-    mkdir ~/serverstatus-monthtraffic && \
-    docker run -d --restart=always --name=serverstatus -v ~/serverstatus-config.json:/ServerStatus/server/config.json -v ~/serverstatus-monthtraffic:/usr/share/nginx/html/json -p 7777:80 -p 35601:35601 cppla/serverstatus:latest
+  if ! is_serverstatus_installed; then
+    install_docker
+
+    wget --no-check-certificate -qO ~/serverstatus-config.json https://raw.githubusercontent.com/cppla/ServerStatus/master/server/config.json && \
+      mkdir ~/serverstatus-monthtraffic && \
+      docker run -d --restart=always --name=serverstatus -v ~/serverstatus-config.json:/ServerStatus/server/config.json -v ~/serverstatus-monthtraffic:/usr/share/nginx/html/json -p 7777:80 -p 35601:35601 cppla/serverstatus:latest
+  fi
 }
 
 # Function to display software installation status
@@ -86,44 +140,24 @@ choose_and_perform_action() {
     if [[ "$choice" =~ ^[0-9]+$ ]] && ((choice >= 0 && choice < ${#actions[@]})); then
       case "${actions[choice]}" in
         "安装 Docker")
-          if is_docker_installed; then
-            echo "Docker 已安装."
-          else
-            install_docker
-            echo -e "\033[1;32mDocker 安装成功.\033[0m"
-          fi
+          install_docker
+          echo -e "\033[1;32mDocker 安装成功.\033[0m"
           ;;
         "安装 Docker Compose")
-          if is_docker_compose_installed; then
-            echo "Docker Compose 已安装."
-          else
-            install_docker_compose
-            echo -e "\033[1;32mDocker Compose 安装成功.\033[0m"
-          fi
+          install_docker_compose
+          echo -e "\033[1;32mDocker Compose 安装成功.\033[0m"
           ;;
         "安装 Portainer")
-          if is_portainer_installed; then
-            echo "Portainer 已安装."
-          else
-            install_portainer
-            echo -e "\033[1;32mPortainer 安装成功.\033[0m"
-          fi
+          install_portainer
+          echo -e "\033[1;32mPortainer 安装成功.\033[0m"
           ;;
         "安装 Nginx Proxy Manager")
-          if is_nginx_proxy_manager_installed; then
-            echo "Nginx Proxy Manager 已安装."
-          else
-            install_nginx_proxy_manager
-            echo -e "\033[1;32mNginx Proxy Manager 安装成功.\033[0m"
-          fi
+          install_nginx_proxy_manager
+          echo -e "\033[1;32mNginx Proxy Manager 安装成功.\033[0m"
           ;;
         "安装 ServerStatus")
-          if is_serverstatus_installed; then
-            echo "ServerStatus 已安装."
-          else
-            install_serverstatus
-            echo -e "\033[1;32mServerStatus 安装成功.\033[0m"
-          fi
+          install_serverstatus
+          echo -e "\033[1;32mServerStatus 安装成功.\033[0m"
           ;;
         "查看安装状态")
           display_software_status
@@ -160,8 +194,8 @@ main() {
   elif is_command_available zypper; then
     sudo zypper refresh && sudo zypper update -y
   else
-    echo -e "\033[1;31m不支持的软件包管理器. 请手动更新系统.\033[0m"
-    exit 1
+    echo "Unsupported package manager."
+    return 1
   fi
 
   # Perform actions
